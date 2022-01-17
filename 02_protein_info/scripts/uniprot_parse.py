69e77fd2a078d33386a876a89c78f61594314e77
Diego Fuentes Palacios <43205584+Dfupa@users.noreply.github.com>
2022-01-17 15:06:32 +0100

    Add files via upload

---
+++ 02_protein_info/scripts/uniprot_parse.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+uniprot_parse.py -- Title of the script
+
+Written by Moisès Bernabeu <mail@mail.com>
+January 2022
+'''
+
+# Import libraries ----
+import sys
+import os
+
+# Path configuration to import utils ----
+filedir = os.path.abspath(__file__)
+projdir = filedir.rsplit('/', 3)[0]
+sys.path.append(projdir)
+
+from utils import *
+
+
+# Definitions ----
+def parse_up(file):
+    protinfo = dict()
+    for line in open(file):
+        if 'DE   ' in line and 'RecName:' in line:
+            # Gene Name
+            prot = line[14:].split('=', 1)[1].replace(';', '')
+            prot = prot.replace('\n', '')
+            protinfo['prot'] = prot
+        elif 'GN   ' in line:
+            # Gene abbreviation
+            for item in line[5:].split(';'):
+                if 'Name=' in item:
+                    protinfo['gene'] = item.replace('Name=', '')
+        elif 'CC   ' in line:
+            if 'FUNCTION: ' in line:
+                toapp = 'fun'
+                line = line[9:].replace('FUNCTION: ', '')
+                protinfo[toapp] = line.replace('\n', '')
+            elif 'LOCATION' in line:
+                toapp = 'loc'
+                line = line[9:].replace('SUBCELLULAR LOCATION: ', '')
+                protinfo[toapp] = line.replace('\n', '')
+            elif ('-!-' in line or 'Copyrighted' in line
+                  or 'Distributed' in line or '--------' in line):
+                toapp = 'Others'
+                protinfo[toapp] = ''
+            else:
+                protinfo[toapp] += line[9:].replace('\n', '')
+
+    protinfo = rmkey(protinfo, 'Others')
+
+    oline = file.rsplit('/', 1)[1].rsplit('.', 1)[0] + '\t'
+    for i, item in enumerate(protinfo):
+        if i != len(protinfo):
+            oline += protinfo[item] + '\t'
+        else:
+            oline += protinfo[item]
+
+    return oline
