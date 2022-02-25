9284fe6fc1fc69a933da38891f3ee268543d82f8
moibernabeu <moigil.bernabeu@gmail.com>
2022-02-25 17:44:12 +0100

    stats update

---
+++ 09_stats/scripts/combined_plots.R
@@ -0,0 +1,61 @@
+# Data analysis of phylome distances - Combined plots
+# Moisès Bernabeu
+# Barcelona, February, 2022
+
+# Libraries loading ----
+library(ggplot2)
+library(ggpubr)
+library(tidyr)
+
+theme_set(theme_bw())
+
+# Loading data ----
+ye_distdf <- read.csv(paste0('../outputs/0005_distdf.csv'),
+                   row.names = 1)
+ye_sp_vs_phy <- read.csv('../outputs/0005_sp_vs_phylome.csv',
+                      row.names = 1)
+ye_sp_vs_phy$group <- factor(ye_sp_vs_phy$group)
+
+hu_distdf <- read.csv(paste0('../outputs/0076_distdf.csv'),
+                      row.names = 1)
+hu_sp_vs_phy <- read.csv('../outputs/0076_sp_vs_phylome.csv',
+                         row.names = 1)
+hu_sp_vs_phy$group <- factor(hu_sp_vs_phy$group)
+
+# Plots ----
+# Joint densities
+a <- ggplot(ye_distdf, aes(x = raw_dist, colour = sp_to)) +
+  geom_density(show.legend = FALSE) +
+  xlim(0, 5) +
+  labs(title = 'YEAST to species')
+
+b <- ggplot(ye_distdf, aes(x = brl_ndist, colour = sp_to)) +
+  geom_density(show.legend = FALSE) +
+  xlim(0, 5) +
+  labs(title = 'YEAST to species')
+
+c <- ggplot(hu_distdf, aes(x = raw_dist, colour = sp_to)) +
+  geom_density(show.legend = FALSE) +
+  xlim(0, 10) +
+  labs(title = 'HUMAN to species')
+
+d <- ggplot(hu_distdf, aes(x = brl_ndist, colour = sp_to)) +
+  geom_density(show.legend = FALSE) +
+  xlim(0, 7) +
+  labs(title = 'HUMAN to species')
+
+ggarrange(a, b, c, d, align = 'hv')
+
+a <- ggplot(ye_sp_vs_phy, aes(dist_sp, brl_ndist, colour = group)) +
+  geom_point(show.legend = FALSE)
+
+b <- ggplot(hu_sp_vs_phy, aes(dist_sp, brl_ndist, colour = group)) +
+  geom_point(show.legend = FALSE)
+
+c <- ggplot(ye_sp_vs_phy, aes(dist_sp, mrca_ndist, colour = group)) +
+  geom_point(show.legend = FALSE)
+
+d <- ggplot(hu_sp_vs_phy, aes(dist_sp, mrca_ndist, colour = group)) +
+  geom_point(show.legend = FALSE)
+
+ggarrange(a, b, c, d, align = 'hv')
--- 09_stats/scripts/phylome_norm.R
+++ 09_stats/scripts/phylome_norm.R
@@ -24,58 +24,84 @@ median_sp <- function(x, df) {
 }
 
 # Normalised distances dataframe generation ----
-dat <- read.csv('../data/0005_dist_noh.csv')
-spdat <- dat[which(dat$mrca_type == 'S' & dat$from_sp == 'YEAST' | 
-                     dat$to_sp == 'YEAST' & dat$from_sp != dat$to_sp), ]
+phylome <- '0076'
+refsp <- 'HUMAN'
+
+dat <- read.csv(paste0('../data/', phylome, '_dist_noh.csv'))
+spdat <- dat[which(dat$mrca_type == 'S' & dat$from_sp == refsp | 
+                     dat$to_sp == refsp & dat$from_sp != dat$to_sp), ]
 meds <- apply(spdat[!duplicated(spdat$tree), c(11:16)], 2, median)
 
 oldvarnms <- names(spdat)[-15][11:15]
 newvarnms <- c('st_ndist', 'mrca_ndist', 'root_ndist',
                'twdth_ndist', 'brl_ndist')
 
-distdf <- data.frame('sp_to' = apply(spdat, 1, get_other, ref = 'YEAST'))
+distdf <- data.frame('sp_to' = apply(spdat, 1, get_other, ref = refsp))
 
 distdf$raw_dist <- spdat$dist
 for (i in 1:length(oldvarnms)) {
-  distdf[, newvarnms[i]] <- spdat$dist / (spdat[, oldvarnms[i]] / meds[oldvarnms[i]])
+  distdf[, newvarnms[i]] <- spdat$dist / (spdat[, oldvarnms[i]] /
+                                            meds[oldvarnms[i]])
 }
 
 distdfg <- gather(distdf, key = 'distance', value = 'value', -sp_to)
 
 # Importing species tree data ----
-spref <- read.csv('../data/0005_sptree_sptree_dist.csv')
-spref <- spref[which(spref$from_sp == 'YEAST' | spref$to_sp == 'YEAST'), ]
-spref$sp_to <- apply(spref, 1, get_other, ref = 'YEAST')
+spref <- read.csv(paste0('../data/', phylome, '_sptree_sptree_dist.csv'))
+spref <- spref[which(spref$from_sp == refsp | spref$to_sp == refsp), ]
+spref$sp_to <- apply(spref, 1, get_other, ref = refsp)
 
 spdists <- data.frame('sp_to' = spref$sp_to, 'dist_sp' = spref$dist)
 phydists <- data.frame(apply(distdf[, -1], 2,
                              FUN = median_sp, df = distdf)[spdists$sp_to, ])
 
-sist_group <- read.csv('../data/0005_sister_group.csv', row.names = 1)
+sist_group <- read.csv(paste0('../data/', phylome, '_sister_group.csv'),
+                       row.names = 1)
 
 sp_vs_phy <- cbind(spdists, phydists,
                    'group' = factor(sist_group[row.names(phydists), ]))
 
+# Writing files ----
+write.csv(distdf, file = paste0('../outputs/', phylome, '_distdf.csv'))
+write.csv(sp_vs_phy, file = paste0('../outputs/', phylome, '_sp_vs_phylome.csv'))
+
 # Plots ----
-pdf('../outputs/0005_all_dens.pdf', width = 6, height = 4)
+distdf <- read.csv(paste0('../outputs/', phylome, '_distdf.csv'),
+                   row.names = 1)
+sp_vs_phy <- read.csv(paste0('../outputs/', phylome, '_sp_vs_phylome.csv'),
+                      row.names = 1)
+sp_vs_phy$group <- factor(sp_vs_phy$group)
+
+pdf(paste0('../outputs/', phylome, '_all_dens.pdf'),
+    width = 6, height = 4)
+ggplot(distdf, aes(x = raw_dist, colour = sp_to)) +
+  geom_density(show.legend = FALSE) +
+  xlim(0, 5) +
+  labs(title = paste0(refsp, ' to species'))
+dev.off()
+
+pdf(paste0('../outputs/', phylome, '_all_norm_dens.pdf'),
+    width = 6, height = 4)
 ggplot(distdf, aes(x = brl_ndist, colour = sp_to)) +
   geom_density(show.legend = FALSE) +
   xlim(0, 7) +
-  labs(title = 'YEAST to species')
+  labs(title = paste0(refsp, ' to species'))
 dev.off()
 
-pdf('../outputs/0005_sp_vs_phylome.pdf', width = 5.5, height = 5)
+pdf(paste0('../outputs/', phylome, '_sp_vs_phylome.pdf'),
+    width = 5.5, height = 5)
 for (varnm in names(sp_vs_phy)[-c(1:2, 9)]) {
   p <- ggplot(sp_vs_phy, aes(x = dist_sp, y = get(varnm), colour = group)) +
     geom_point() +
     ylab(varnm) +
-    labs(title = 'YEAST to species') +
-    theme(legend.position = 'bottom')
+    theme(legend.position = 'bottom') +
+    labs(title = paste0(refsp, ' to species'))
   print(p)
 }
 dev.off()
 
-pdf('../outputs/0005_dist_dens.pdf', width = 10, height = 6)
+pdf(paste0('../outputs/', phylome, '_dist_dens.pdf'),
+    width = 10, height = 6)
 for (varnm in names(distdf)[-1]) {
   p <- ggplot(distdf, aes(x = get(varnm), colour = sp_to, fill = sp_to)) +
     geom_density(show.legend = FALSE, alpha = 0.6) +
