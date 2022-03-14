43be8c7099355e1544587f0df0b0427d68d4f8c2
moibernabeu <moigil.bernabeu@gmail.com>
2022-03-14 08:31:09 +0100

    clean repo

--- 04_seq2seq_dists/scripts/get_cmdlist.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-files='../data/*'
-
-for file in $files
-do
-  ph_no=$(echo $file | rev | cut -d '/' -f1 | rev | cut -d '_' -f1)
-  echo python3 seq2seq_dists.py -f $file -o ../outputs -p ../data/${ph_no}_all_protein_names.txt -c 4
-done
