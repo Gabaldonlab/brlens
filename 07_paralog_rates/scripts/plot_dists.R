b2d0f96de792b8eecfc7eca23f2ebba06530a768
moibernabeu <moigil.bernabeu@gmail.com>
2022-04-15 19:42:55 +0200

    plots

--- 07_paralog_rates/scripts/plot_dists.R
+++ 07_paralog_rates/scripts/plot_dists.R
@@ -4,8 +4,15 @@ theme_set(theme_bw())
 
 dat <- read.csv('../outputs/0076_dist.csv')
 
-ggplot(dat, aes(norm_median, norm_D / norm_S)) +
-  geom_point()
+cols <- c('Mean' = 'steelblue', 'Median' = 'darkorange3')
+ggplot(dat, aes(norm_D / norm_S, norm_median)) +
+  geom_point() +
+  geom_hline(yintercept = mean(dat$norm_median), linetype = 'Mean', col = 'steelblue') +
+  geom_hline(yintercept = median(dat$norm_median), linetype = 'Median', col = 'darkorange3') +
+  labs(color = 'Legend') +
+  scale_color_manual(values = colors) +
+  scale_linetype_manual(name = "limit", values = c(2, 4), 
+                        guide = guide_legend(override.aes = list(color = c('steelblue', 'darkorange3'))))
 
 gdupl <- gather(dat[, c('max_median', 'min_median')])
 
@@ -41,7 +48,8 @@ gdupl <- gather(dat[, c('max_median', 'min_median')])
 
 ggplot(gdupl, aes(y = value, x = key, col = key, fill = key)) +
   geom_boxplot(alpha = 0.6) +
-  ylim(0, quantile(gdupl$value, 0.9))
+  ylim(0, quantile(gdupl$value, 0.9)) +
+  geom_hline(yu)
 
 gdupl <- gather(dat[, c('max_mean', 'min_mean')])
 
