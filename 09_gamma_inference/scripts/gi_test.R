22e146c30ab3f53e44f29fe95f82166575e1209e
moibernabeu <moigil.bernabeu@gmail.com>
2022-05-09 17:03:05 +0200

    gamma inference jags

--- 09_gamma_inference/scripts/gi_test.R
+++ 09_gamma_inference/scripts/gi_test.R
@@ -4,8 +4,18 @@
 
 library(ggplot2)
 library(ggpubr)
+library(rjags)
+# library(DBDA2E)
+library(coda)
+library(ggmcmc)
+library(tidyr)
+library(wesanderson)
 
 theme_set(theme_bw())
+
+# Definitions ----
+# Basic custom palette
+pal <- c('steelblue', 'darkorange3', 'chartreuse3')
 
 # Likelihood
 y <- rgamma(100, 2, 4)
@@ -46,4 +56,71 @@ s1000 <- ggplot(like, aes(alpha, beta, fill = like)) +
 ggarrange(s100, s1000)
 
 
-# Posterior gibbs
+# Posterior gibbs ----
+# With jags
+
+model_text <- 'model{
+  # Likelihood
+  for (i in 1:n) {
+    y[i] ~ dgamma(a, b)
+  }
+  
+  # Prior distributions
+  a ~ dunif(0, 100)
+  b ~ dunif(0, 100)
+}'
+
+y <- rgamma(1000, 2, 4)
+
+datlist <- list(y = y, n = length(y))
+
+model_jags <- jags.model(textConnection(model_text), 
+                         data = datlist,
+                         n.chains = 3)
+
+post <- coda.samples(model_jags, 
+                     variable.names = c('a', 'b'), 
+                     n.iter = 1000)
+
+summary(post)
+
+post_list <- mcmc.list(post)
+post_df <- as.data.frame(ggs(post_list))
+post_df$Chain <- as.factor(post_df$Chain)
+
+acarr <- autocorr(post, lags = 1:5)
+
+autocorr.plot(post)
+
+ggs_diagnostics(post_df)
+
+ggplot(post_df, aes(x = value, colour = Chain)) +
+  geom_density() +
+  geom_point(aes(y = 0), alpha = 0.1) +
+  facet_wrap(~Parameter, scales = 'free') +
+  scale_color_manual(values = wes_palette('FantasticFox1'))
+
+ggplot(post_df, aes(x = Iteration, y = value, colour = Chain)) +
+  geom_line() +
+  facet_wrap(~Parameter, scales = 'free') +
+  scale_color_manual(values = wes_palette('FantasticFox1'))
+
+autocor_df <- data.frame()
+for (chain in as.numeric(levels(post_df$Chain))) {
+  for (param  in levels(post_df$Parameter)) {
+    autocor_df <- rbind(autocor_df,
+                        data.frame(Chain = paste(chain), Parameter = param,
+                                   ac(post_df[which(post_df$Chain == chain &
+                                                      post_df$Parameter == param),
+                                              'value'],
+                             nLags = 1000), stringsAsFactors = TRUE))
+  }
+}
+
+ggplot(autocor_df, aes(x = Lag, y = Autocorrelation, colour = Chain)) +
+  geom_line() +
+  facet_grid(Parameter ~ Chain) +
+  # xlim(0, 50) +
+  scale_color_manual(values = wes_palette('FantasticFox1'))
+
+ggs_running(post_df)
