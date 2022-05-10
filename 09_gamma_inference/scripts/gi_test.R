d9f7ff4a1e41097a41ab715a64836d1e848571ca
moibernabeu <moigil.bernabeu@gmail.com>
2022-05-10 18:41:10 +0200

    JAGS

--- 09_gamma_inference/scripts/gi_test.R
+++ 09_gamma_inference/scripts/gi_test.R
@@ -19,6 +19,98 @@ theme_set(theme_bw())
 # Basic custom palette
 pal <- c('steelblue', 'darkorange3', 'chartreuse3')
 
+# Posterior gibbs ----
+# With jags
+
+model_text <- 'model{
+  # Likelihood
+  for (i in 1:n) {
+    y[i] ~ dgamma(a, b)
+  }
+  
+  m <- a / b
+  v <- a / b^2
+  mo <- (a - 1) / b
+  
+  # Prior distributions
+  a ~ dunif(0, 100)
+  b ~ dunif(0, 100)
+}'
+
+y <- rgamma(1000, 2, 4)
+
+datlist <- list(y = y, n = length(y))
+
+model_jags <- jags.model(textConnection(model_text), 
+                         data = datlist,
+                         n.chains = 3)
+
+post <- coda.samples(model_jags, 
+                     variable.names = c('a', 'b', 'm', 'v', 'mo'), 
+                     n.iter = 10000)
+
+summary(post)
+
+post_list <- mcmc.list(post)
+post_df <- as.data.frame(ggs(post_list))
+post_df$Chain <- as.factor(post_df$Chain)
+
+acarr <- autocorr(post, lags = 1:5)
+
+autocorr.plot(post)
+
+ggs_diagnostics(post_df)
+
+ggplot(post_df, aes(x = value, colour = Chain)) +
+  geom_density() +
+  # geom_point(aes(y = 0), alpha = 0.1) +
+  facet_wrap(~Parameter, scales = 'free') +
+  scale_color_manual(values = wes_palette('FantasticFox1'))
+
+ggplot(post_df, aes(x = Iteration, y = value, colour = Chain)) +
+  geom_line() +
+  facet_wrap(~Parameter, scales = 'free') +
+  scale_color_manual(values = wes_palette('FantasticFox1'))
+
+D <- post_df
+
+nLags <- 250
+wc.ac <- D %>%
+  group_by(Parameter, Chain) %>%
+  do(ac(.$value, nLags))
+
+ggplot(autocor_df, aes(x = Lag, y = Autocorrelation, colour = Chain)) +
+  geom_line() +
+  facet_grid(Parameter ~ Chain) +
+  # xlim(0, 50) +
+  scale_color_manual(values = wes_palette('FantasticFox1'))
+
+# Estimates of the mean by iteration
+dm.m <- D %>%
+  dplyr::group_by(Parameter, Chain) %>%
+  dplyr::summarize(m=mean(value))
+# Calculate the running mean
+# Force the object to be sorted by Parameter, and hence avoid 'rm' calculation
+# to be wrong
+dm.rm <- D %>%
+  arrange(Parameter, Iteration) %>%
+  group_by(Parameter, Chain) %>%
+  mutate(rm = cumsum(value) / Iteration,
+         sd = sqrt(cumvar(value)),
+         up = rm + sd,
+         do = rm - sd)
+
+ggplot(dm.rm, aes(x = Iteration, y = rm, colour = as.factor(Chain))) +
+  geom_line() +
+  # geom_line(aes(y = up), lty = 3, show.legend = FALSE) +
+  # geom_line(aes(y = do), lty = 3, show.legend = FALSE) +
+  facet_grid(Parameter ~ Chain, scales = 'free') +
+  geom_hline(aes(yintercept = m), dm.m, lty = 4, size = 0.3) +
+  ylab('Running mean') +
+  labs(colour = 'Chain')
+
+ggs_running(post_df)
+
 # Likelihood
 y <- rgamma(100, 2, 4)
 
@@ -57,101 +149,5 @@ s1000 <- ggplot(like, aes(alpha, beta, fill = like)) +
 
 ggarrange(s100, s1000)
 
-
-# Posterior gibbs ----
-# With jags
-
-model_text <- 'model{
-  # Likelihood
-  for (i in 1:n) {
-    y[i] ~ dgamma(a, b)
-  }
-  
-  m <- a / b
-  v <- a / b^2
-  mo <- (a - 1) / b
-  
-  # Prior distributions
-  a ~ dunif(0, 100)
-  b ~ dunif(0, 100)
-}'
-
-y <- rgamma(1000, 2, 4)
-
-datlist <- list(y = y, n = length(y))
-
-model_jags <- jags.model(textConnection(model_text), 
-                         data = datlist,
-                         n.chains = 3)
-
-post <- coda.samples(model_jags, 
-                     variable.names = c('a', 'b', 'm', 'v', 'mo'), 
-                     n.iter = 1000)
-
-summary(post)
-
-post_list <- mcmc.list(post)
-post_df <- as.data.frame(ggs(post_list))
-post_df$Chain <- as.factor(post_df$Chain)
-
-acarr <- autocorr(post, lags = 1:5)
-
-autocorr.plot(post)
-
-ggs_diagnostics(post_df)
-
-ggplot(post_df, aes(x = value, colour = Chain)) +
-  geom_density() +
-  # geom_point(aes(y = 0), alpha = 0.1) +
-  facet_wrap(~Parameter, scales = 'free') +
-  scale_color_manual(values = wes_palette('FantasticFox1'))
-
-ggplot(post_df, aes(x = Iteration, y = value, colour = Chain)) +
-  geom_line() +
-  facet_wrap(~Parameter, scales = 'free') +
-  scale_color_manual(values = wes_palette('FantasticFox1'))
-
-autocor_df <- data.frame()
-for (chain in as.numeric(levels(post_df$Chain))) {
-  for (param  in levels(post_df$Parameter)) {
-    autocor_df <- rbind(autocor_df,
-                        data.frame(Chain = paste(chain), Parameter = param,
-                                   ac(post_df[which(post_df$Chain == chain &
-                                                      post_df$Parameter == param),
-                                              'value'],
-                             nLags = 1000), stringsAsFactors = TRUE))
-  }
-}
-
-ggplot(autocor_df, aes(x = Lag, y = Autocorrelation, colour = Chain)) +
-  geom_line() +
-  facet_grid(Parameter ~ Chain) +
-  # xlim(0, 50) +
-  scale_color_manual(values = wes_palette('FantasticFox1'))
-
-# Calculate the mean of the chain
-D <- post_df
-dm.m <- D %>%
-  dplyr::group_by(Parameter, Chain) %>%
-  dplyr::summarize(m=mean(value))
-# Calculate the running mean
-# Force the object to be sorted by Parameter, and hence avoid 'rm' calculation
-# to be wrong
-dm.rm <- D %>%
-  arrange(Parameter, Iteration) %>%
-  group_by(Parameter, Chain) %>%
-  mutate(rm = cumsum(value) / Iteration,
-         sd = sqrt(cumvar(value)),
-         up = rm + sd,
-         do = rm - sd)
-
-ggplot(dm.rm, aes(x = Iteration, y = rm, colour = as.factor(Chain))) +
-  geom_line() +
-  geom_line(aes(y = up), lty = 3, show.legend = FALSE) +
-  geom_line(aes(y = do), lty = 3, show.legend = FALSE) +
-  facet_grid(Parameter ~ Chain, scales = 'free') +
-  geom_hline(aes(yintercept = m), dm.m, lty = 4, size = 0.3) +
-  ylab('Running mean') +
-  labs(colour = 'Chain')
-
-ggs_running(post_df)
+ggs_autocorrelation(post_df)
+ggs_autocorrelation
